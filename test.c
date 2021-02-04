1e39fe3dbeee35519903b0ffca8c9219422cc321
Ankur <ankuryd@iitk.ac.in>
2021-02-04 17:55:28 +0530

    Final updates

--- test.c
+++ test.c
@@ -2,23 +2,16 @@
 
 int main(){
     // this is comment
-    /* ***i
+    /* ***
     chefe
     r***** */
-    
-    
-    
-    
-    int **p2;
     int a=-1.2,b=-2;
     int new[10];
-    int int123,c;
+    int c;
     c=a+b;
-    if(2==1){
-        printf("OKd %d",c);
+    if(2>1){
+        printf("OKd");
     }
-    int* pot;
-    int w,*no;
     printf("%d",c);
     return 0;
 }
